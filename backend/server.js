71fe58e29b6bdfb313d13429f6738dd323d0bc33
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-02-08 11:26:43 +0330

    feat(server.js errormiddleware.js): setup server and added default error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-import express from "express";
+import express from 'express'
+import dotenv from 'dotenv'
+import { notFound, errorHandler } from '../middleware/errorMiddleware.js'
+import userRoutes from './routes/userRoutes.js'
+dotenv.config()
+
+const port = process.env.PORT || 5000
+const app = express()
+
+app.use('/api/users', userRoutes)
+
+app.get('/', (req, res) => res.send('server is ready'))
+app.use(notFound)
+app.use(errorHandler)
+app.listen(port, () => console.log(`server started on port ${port}`))
