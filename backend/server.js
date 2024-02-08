f77007c3ae29b4ebd4acdf7e03858e04cd71cb80
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-02-08 20:46:13 +0330

    feat(usermodel.js usercontrollers.js): created user schema and encrypted password before going to DB
    also created register user route

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ import connectDB from './config/db.js'
 const port = process.env.PORT || 5000
 connectDB()
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/users', userRoutes)
 
