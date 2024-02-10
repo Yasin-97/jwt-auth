0f5930629e16746a6d53dd6b250e9e848b0344c8
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-02-10 13:23:32 +0330

    feat(frontend): delete default files and costumize the frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    prot: 3000,
+    port: 3000,
     Proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:8000',
         changeOrigin: true,
       },
     },
