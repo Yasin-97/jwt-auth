2f3c3141fcd84fe536bae7c8dfa51263bb9ae279
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-02-10 16:48:31 +0330

    feat(store.js): setup redux store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import store from '../store.js'
+import { Provider } from 'react-redux'
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -10,17 +12,23 @@ import {
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import Home from './screen/Home.jsx'
+import Login from './screen/Login.jsx'
+import Register from './screen/Register.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Route>
   )
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 )
