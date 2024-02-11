128d8731327e3e372e48ae086aa12a42324fa220
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-02-11 14:13:37 +0330

    feat(server.js): installed concurrently and cors

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ import dotenv from 'dotenv'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import userRoutes from './routes/userRoutes.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 dotenv.config()
 import connectDB from './config/db.js'
 
 const port = process.env.PORT || 5000
 connectDB()
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
