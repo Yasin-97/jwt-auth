13866256256043426b77bbcc6fae26688e9870f3
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-02-11 14:20:45 +0330

    feat(login.jsx): setup lgoin functionality

--- frontend/src/screen/Login.jsx
+++ frontend/src/screen/Login.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { Form, Button, Row, Col } from 'react-bootstrap'
 import FormContainer from '../components/FormContainer'
-
+import { useDispatch, useSelector } from 'react-redux'
+import { useLoginMutation } from '../slices/usersApiSlice'
+import { setCredentials } from '../slices/authSlice'
+import { toast } from 'react-toastify'
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const [login, { isLoading }] = useLoginMutation()
+  const { userInfo } = useSelector(state => state.auth)
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate('/')
+    }
+  }, [navigate, userInfo])
   const submitHandler = async e => {
-    e.prevnetDefault()
-    console.log('submit')
+    e.preventDefault()
+    try {
+      const res = await login({ email, password }).unwrap()
+      dispatch(setCredentials({ ...res }))
+      navigate('/')
+    } catch (err) {
+      console.log('error', err)
+      toast.error(err?.data?.message || err.error)
+    }
   }
 
   return (
